00d970c180b32da5f5f44b11dcac8cda4f96862f
Justin Lopez <justincolelopez1@gmail.com>
2025-08-29 15:07:40 -0400

    add new mcu project, its building with the new mcu

--- README.md
+++ README.md
@@ -3,5 +3,6 @@ I'll put some videos in here too it'll be pretty
 
 TODO:
 add clang tidy
+add workflow that runs clang format, and builds the projects
 add can monitoring board as project (new mcu)
 add a folder for our drivers and make sure its able to be included easily
--- firmware/NUCLEOL476RG-FreeRTOS-ExampleProject/user/src/test.cpp
+++ firmware/NUCLEOL476RG-FreeRTOS-ExampleProject/user/src/test.cpp
@@ -3,8 +3,16 @@
 #include "stm32l4xx_hal.h"
 #include "main.h"
 
+#include <atomic>
+
+class fart {
+
+};
+
 void StartDefaultTask_user(void *argument) {
+    std::atomic<int> t;
     for (;;) {
+        volatile fart f;
         HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
         osDelay(500);
     }
