eb7f12d8b36c146563b88b3788a5c940a8bd9fa6
Justin Lopez <justincolelopez1@gmail.com>
2025-08-30 12:51:28 -0400

    update the clang format

--- firmware/NUCLEOL476RG-FreeRTOS-ExampleProject/user/src/test.cpp
+++ firmware/NUCLEOL476RG-FreeRTOS-ExampleProject/user/src/test.cpp
@@ -1,12 +1,15 @@
-#include "cmsis_os2.h"  // this works cause cmsis_os includes cmsis_os2 so u could write either
-#include "main.h"
-#include "stm32l4xx_hal.h"
-
 #include "test.hpp"
 
-void StartDefaultTask_user(void *argument) {
-    for (;;) {
-        HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin); // blink led
+#include <cmsis_os2.h>
+#include <stm32l4xx_hal.h>
+
+#include "main.h"
+
+void StartDefaultTask_user(void *argument)
+{
+    for (;;)
+    {
+        HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);  // blink led
         osDelay(500);
     }
 }
