7fbf9ba4b586af0f659edcb938d4f6f6854b3196
Justin Lopez <justincolelopez1@gmail.com>
2025-09-10 13:54:48 -0400

    added more helpers and started sendbits over spi function which is gonna change

--- resources/Drivers/SG_Drivers/eeprom/inc/eeprom_93aa46.hpp
+++ resources/Drivers/SG_Drivers/eeprom/inc/eeprom_93aa46.hpp
@@ -12,12 +12,14 @@
 #error "Define your STM32 part macro (e.g., STM32U575xx or STM32L476xx)."
 #endif
 
+// this eeprom has a start bit before it begins reading bits it recieves on its mosi line, which means you can prepend as many zeros as you like so that it correctly aligns with the 8 bit at a time send protocol of the HAL_SPI functions
 class Eeprom93AA46 final : public Eeprom
 {
    public:
     Eeprom93AA46(SPI_HandleTypeDef* hspi, GPIO_TypeDef* cs_port, uint16_t cs_pin)
         : hspi_(hspi), cs_port_(cs_port), cs_pin_(cs_pin)
     {
+        csLow();
     }
 
     int read(uint32_t addr, void* buf, size_t len) override;
@@ -29,13 +31,37 @@ class Eeprom93AA46 final : public Eeprom
 
    private:
     SPI_HandleTypeDef* hspi_;
-
     GPIO_TypeDef* cs_port_;
     uint16_t cs_pin_;
 
-    inline void csLow(){};
-    inline void csHigh(){};
+    // constant opcodes
+    static constexpr uint8_t kOpcodeRead = 0b10;
+    static constexpr uint8_t kOpcodeWrite = 0b01;
+    static constexpr uint8_t kOpcodeGeneral = 0b00;
 
-    static constexpr uint8_t OpRead = 0b10;
-    static constexpr uint8_t OpWrite = 0b01;
+    // constand address's, specifically for use with the general opcode
+    static constexpr uint8_t kAddrEWDS = 0b0000000;
+    static constexpr uint8_t kAddrEWEN = 0b1100000;
+
+    static constexpr size_t kEWInstructionLength = 10;
+    static constexpr size_t kRWInstructionLength = 18;
+
+    // cs control helpers
+    inline void csLow() { HAL_GPIO_WritePin(cs_port_, cs_pin_, GPIO_PIN_RESET); };
+    inline void csHigh() { HAL_GPIO_WritePin(cs_port_, cs_pin_, GPIO_PIN_SET); };
+
+    // helper functions that send one of the commands to the eeprom
+    HAL_StatusTypeDef sendEWEN();
+    HAL_StatusTypeDef sendEWDS();
+    HAL_StatusTypeDef sendRead(uint32_t addr, uint8_t& out);
+    HAL_StatusTypeDef sendWrite(uint32_t addr, const uint8_t& byte);
+
+    // function should be used to send a single command
+    // count is in bits. Prepends zeros so that it aligns
+    // cannot send more than 32 bits
+    // basically expecting for the count variable to be either 10 or 18 from the constants above
+    //
+
+    // TODO: Change count variable to instruction type, and function to be one that sends a specific instruction to the eeprom
+    HAL_StatusTypeDef sendBits(const uint32_t& data, size_t count);
 };
--- resources/Drivers/SG_Drivers/eeprom/src/eeprom_93aa46.cpp
+++ resources/Drivers/SG_Drivers/eeprom/src/eeprom_93aa46.cpp
@@ -1,11 +1,64 @@
 #include "eeprom_93aa46.hpp"
 
+#include <array>
+
+// read can only do one byte at a time on on this device
 int Eeprom93AA46::read(uint32_t addr, void* buf, size_t len)
+{
+    // write one byte at a time
+    auto* byte_arr = static_cast<const uint8_t*>(buf);
+    for (int i = 0; i < len; i++)
+    {
+        // begin write sequence for bytes
+    }
+    return 0;
+}
+
+// write can only do one byte at a time on on this device
+int Eeprom93AA46::write(uint32_t addr, const void* buf, size_t len)
 {
     return 0;
 }
 
-int Eeprom93AA46::write(uint32_t addr, const void* buf, size_t len)
+HAL_StatusTypeDef Eeprom93AA46::sendEWEN() {}
+
+HAL_StatusTypeDef Eeprom93AA46::sendEWDS()
 {
-    return 0;
+    return HAL_StatusTypeDef();
+}
+
+HAL_StatusTypeDef Eeprom93AA46::sendRead(uint32_t addr, uint8_t& out)
+{
+    return HAL_StatusTypeDef();
+}
+
+HAL_StatusTypeDef Eeprom93AA46::sendWrite(uint32_t addr, const uint8_t& byte)
+{
+    return HAL_StatusTypeDef();
+}
+
+HAL_StatusTypeDef Eeprom93AA46::sendBits(const uint32_t& data, size_t count)
+{
+    if (count > 32)
+    {
+        return HAL_StatusTypeDef();
+    }
+
+    std::array<uint8_t, 4> bytes_arr;
+
+    bytes_arr[0] = static_cast<uint8_t>(data >> 24);
+    bytes_arr[1] = static_cast<uint8_t>(data >> 16);
+    bytes_arr[2] = static_cast<uint8_t>(data >> 8);
+    bytes_arr[3] = static_cast<uint8_t>(data >> 0);
+
+    uint8_t* start = &bytes_arr[0];
+    size_t byte_count = 4;
+
+    if (count <= 16)
+    {
+        start += 2;
+        byte_count = 2;
+    }
+
+    HAL_SPI_Transmit(hspi_, start, byte_count, 100);
 }
