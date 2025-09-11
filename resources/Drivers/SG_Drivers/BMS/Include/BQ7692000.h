d8a77e345837ecd8adc108e57976e94763e010b1
Jackb18b <jackb18b@gmail.com>
2025-09-11 12:24:48 -0400

    finalized get/set functions

--- resources/Drivers/SG_Drivers/BMS/Include/BQ7692000.h
+++ resources/Drivers/SG_Drivers/BMS/Include/BQ7692000.h
@@ -15,8 +15,16 @@ extern "C"
 #endif
 }  // Extern "C"
 
+#define ONE_BYTE 1
 #define TWO_BYTES 2
 #define CELL_COUNT 5
+
+#define CC_CONFIG_MASK 0x19
+#define STATUS_CC_READY_MASK 0b1000'0000
+#define CC_EN_MASK 0b0100'0000
+#define ADC_EN_MASK 0b0001'0000
+#define STATUS_ERR_MASK 0b0010'1111
+#define ACTIVE_BAL_MASK 0b0001'1111
 
 // TODO: Split I2C Device into own folders and expand default operations
 class I2CDevice
@@ -48,24 +56,76 @@ class BQ7692000PW : public I2CDevice
         dataVC_.reserve(CELL_COUNT * TWO_BYTES);  // 5 cells, 2 bytes per
     };
 
+    /**
+     * @brief Enables Coulomb Counting and ADC reading on VC pins
+     *
+     * @return HAL_StatusTypeDef
+     */
+    HAL_StatusTypeDef init();
+    // TODO: Possibly make switch into SHIP mode for power conservation ?
+
+    /**
+     * @brief Reads the 5 Cell voltages of the BQ7692x
+     *
+     * @param vc_values list of cell voltages
+     * @return HAL_StatusTypeDef
+     */
     HAL_StatusTypeDef getVC(std::vector<uint16_t> &vc_values);
+
+    /**
+     * @brief Reads Coulomb count register after ensuring CC_Ready is true
+     *
+     * @param [out] data Atomic value of CC
+     * @return HAL_StatusTypeDef
+     */
     HAL_StatusTypeDef getCC(uint16_t *data);
 
+    /**
+     * @brief
+     *
+     * @param data
+     * @return HAL_StatusTypeDef
+     */
+    HAL_StatusTypeDef getBAT(uint16_t *data);
+
+    HAL_StatusTypeDef getADCOffset(uint8_t *data);
+    HAL_StatusTypeDef setADCGain(uint8_t *data);
+
+    HAL_StatusTypeDef getActiveBalancing(uint8_t *activeBal);
+    HAL_StatusTypeDef setActiveBalancing(uint8_t *activeBal);
+
    private:
+    /* VARIABLES */
+
+    // TODO: Consider cleaning up this section
     static constexpr uint8_t i2c_addr = (0x08 << 1);
+
     enum class registers : uint8_t
     {
-        status = 0x00,
-        vc1_hi = 0x0C,
-        cc_hi = 0x32
+        SYS_STAT = 0x00,
+        CELL_BAL1 = 0x01,
+        SYS_CTRL1 = 0x04,
+        SYS_CTRL2 = 0x05,
+        CC_CFG = 0x0B,
+        VC1_HI = 0x0C,
+        BAT_HI = 0x2A,
+        CC_HI = 0x32,
+        ADCGAIN1 = 0x50,
+        ADCOFFSET = 0x51,
+        ADCGAIN2 = 0x59
     };
-
-    static constexpr uint8_t STATUS_CC_READY_MASK = 0b1000'0000;
-    static constexpr uint8_t STATUS_ERR_MASK = 0b0010'1111;
 
     uint8_t status_ = 0;
     uint8_t dataCC_[TWO_BYTES] = {};
+    uint8_t dataBAT_[TWO_BYTES] = {};
     std::vector<uint8_t> dataVC_;
+
+    /* FUNCTIONS */
+
+    HAL_StatusTypeDef initCC();
+
+    HAL_StatusTypeDef enableCCReading();
+    HAL_StatusTypeDef enableADC();
 
     HAL_StatusTypeDef checkStatus();
     HAL_StatusTypeDef checkVC();
--- resources/Drivers/SG_Drivers/BMS/Src/BQ7692000.cpp
+++ resources/Drivers/SG_Drivers/BMS/Src/BQ7692000.cpp
@@ -9,6 +9,15 @@
             return HAL_ERROR; \
     } while (0)
 
+HAL_StatusTypeDef BQ7692000PW::init()
+{
+    TRY(initCC());
+    TRY(enableCCReading());
+    TRY(enableADC());
+
+    return HAL_OK;
+}
+
 HAL_StatusTypeDef BQ7692000PW::getCC(uint16_t *data)
 {
     TRY(checkCC());
@@ -31,9 +40,52 @@ HAL_StatusTypeDef BQ7692000PW::getVC(std::vector<uint16_t> &vc_values)
     return HAL_OK;
 }
 
+HAL_StatusTypeDef BQ7692000PW::getBAT(uint16_t *data)
+{
+    TRY(readN(static_cast<uint8_t>(registers::BAT_HI), dataBAT_, TWO_BYTES));
+
+    *data = (dataBAT_[0] << 8) | dataBAT_[1];
+    return HAL_OK;
+}
+
+HAL_StatusTypeDef BQ7692000PW::getActiveBalancing(uint8_t *activeBal)
+{
+    TRY(readN(static_cast<uint8_t>(registers::CELL_BAL1), activeBal, ONE_BYTE));
+
+    return HAL_OK;
+}
+
+HAL_StatusTypeDef BQ7692000PW::setActiveBalancing(uint8_t *activeBal)
+{
+    // Ensure upper three bits are not written
+    uint8_t write = *activeBal & ACTIVE_BAL_MASK;
+    TRY(readN(static_cast<uint8_t>(registers::CELL_BAL1), &write, ONE_BYTE));
+
+    return HAL_OK;
+}
+
+HAL_StatusTypeDef BQ7692000PW::setADCGain(uint8_t *data)
+{
+    uint8_t formatted_data = *data & 0x1F;
+    uint8_t reg1 = (formatted_data & 0b0001'1000) >> 1;
+    uint8_t reg2 = (formatted_data & 0b0000'0111) << 5;
+
+    TRY(writeN(static_cast<uint8_t>(registers::ADCGAIN1), &reg1, ONE_BYTE));
+    TRY(writeN(static_cast<uint8_t>(registers::ADCGAIN2), &reg2, ONE_BYTE));
+
+    return HAL_OK;
+}
+
+HAL_StatusTypeDef BQ7692000PW::getADCOffset(uint8_t *data)
+{
+    TRY(readN(static_cast<uint8_t>(registers::ADCOFFSET), data, ONE_BYTE));
+
+    return HAL_OK;
+}
+
 HAL_StatusTypeDef BQ7692000PW::checkStatus()
 {
-    TRY(readN(static_cast<uint8_t>(registers::status), &status_, 1));
+    TRY(readN(static_cast<uint8_t>(registers::SYS_STAT), &status_, ONE_BYTE));
 
     if ((status_ & STATUS_ERR_MASK) != 0)
     {
@@ -43,11 +95,35 @@ HAL_StatusTypeDef BQ7692000PW::checkStatus()
     return HAL_OK;
 }
 
+HAL_StatusTypeDef BQ7692000PW::initCC()
+{
+    uint8_t data = CC_CONFIG_MASK;
+    TRY(writeN(static_cast<uint8_t>(registers::CC_CFG), &data, ONE_BYTE));
+
+    return HAL_OK;
+}
+
+HAL_StatusTypeDef BQ7692000PW::enableCCReading()
+{
+    uint8_t data = CC_EN_MASK;
+    TRY(writeN(static_cast<uint8_t>(registers::SYS_CTRL2), &data, ONE_BYTE));
+
+    return HAL_OK;
+}
+
+HAL_StatusTypeDef BQ7692000PW::enableADC()
+{
+    uint8_t data = ADC_EN_MASK;
+    TRY(writeN(static_cast<uint8_t>(registers::SYS_CTRL1), &data, ONE_BYTE));
+
+    return HAL_OK;
+}
+
 HAL_StatusTypeDef BQ7692000PW::checkVC()
 {
     for (int i = 0; i < 5; i++)
     {
-        TRY(readN(static_cast<uint8_t>(registers::vc1_hi) + static_cast<uint8_t>(i * TWO_BYTES),
+        TRY(readN(static_cast<uint8_t>(registers::VC1_HI) + static_cast<uint8_t>(i * TWO_BYTES),
                   dataVC_.data() + (i * TWO_BYTES),
                   TWO_BYTES));
     }
@@ -63,5 +139,10 @@ HAL_StatusTypeDef BQ7692000PW::checkCC()
     {
         return HAL_BUSY;  // Maybe HAL_ERROR?
     }
-    return readN(static_cast<uint8_t>(registers::cc_hi), dataCC_, static_cast<size_t>(TWO_BYTES));
+
+    TRY(readN(static_cast<uint8_t>(registers::CC_HI), dataCC_, static_cast<size_t>(TWO_BYTES)));
+
+    // Clear CC Ready latch
+    uint8_t clearCC = STATUS_CC_READY_MASK;
+    return writeN(static_cast<uint8_t>(registers::SYS_STAT), &clearCC, ONE_BYTE);
 }
