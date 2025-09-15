3c1d37937b8e704222c1ff1d6534a125f4428e07
somniwolf <danieltakshi88@gmail.com>
2025-09-15 18:10:17 -0400

    add voltage reading

--- firmware/LTC6810HG Driver/Core/Inc/LTC6810.hpp
+++ firmware/LTC6810HG Driver/Core/Inc/LTC6810.hpp
@@ -3,6 +3,10 @@
 class LTC6810 {
 public:
 	LTC6810(SPI_HandleTypeDef *phspi_, GPIO_TypeDef *pgpio_port, uint16_t cs_pin, uint8_t num_devices);
+
+	HAL_StatusTypeDef ReadVoltages();
+
+
 	uint16_t calculatePEC(uint8_t *pData , int len);
 private:
 
@@ -10,7 +14,10 @@ private:
 
 	HAL_StatusTypeDef WriteRegister(uint8_t cmdByteHigh, uint8_t cmdByteLow, uint8_t *dataBytes, uint8_t dataLen);
 	HAL_StatusTypeDef ReadRegister(uint8_t cmdByteHigh, uint8_t cmdByteLow, uint8_t *receiveBytes, uint8_t receiveLen);
+	HAL_StatusTypeDef SendCommand(uint8_t cmdByteHigh, uint8_t cmdByteLow);
 	HAL_StatusTypeDef WakeDevices();
+
+	uint16_t *pCellVoltages;
 
 	SPI_HandleTypeDef *phspi;
 	GPIO_TypeDef *pgpioPort;
--- firmware/LTC6810HG Driver/Core/Src/LTC6810.cpp
+++ firmware/LTC6810HG Driver/Core/Src/LTC6810.cpp
@@ -1,4 +1,5 @@
 #include "LTC6810.hpp"
+#include "LTC6810Registers.hpp"
 
 int16_t pec15Table[256];
 int16_t CRC15_POLY = 0x4599;
@@ -9,6 +10,50 @@ LTC6810::LTC6810(SPI_HandleTypeDef *phspi_, GPIO_TypeDef *pgpio_port, uint16_t c
 	InitPEC15Table();
 	WakeDevices();
 }
+
+HAL_StatusTypeDef LTC6810::ReadVoltages(){
+	HAL_StatusTypeDef status;
+
+
+
+	// cells 1 through 3 and 4 through 6 are two different registers, and each value is sent as two bytes
+	// this code combines 2 bytes into 1 uint16, and also interweaves values so first 6 voltages in pCellVoltages belong to device 1, next 6 belong to device 2 etc
+
+	// start ADC conversions
+	status = SendCommand(LTC6810_REG_ADCV_H, LTC6810_REG_ADCV_L);
+	if (status != HAL_OK){return status;}
+	HAL_Delay(2); // max time for ADC to finish is about 1.3 ms
+
+
+	// code below is unecessary and probably wouldn't work bc data line should be read later than for a normal spi command
+//	uint8_t *pollADC = nullptr;
+//	while (*pollADC != 0b11111111){
+//		HAL_Delay(1);
+//		status = ReadRegister(LTC6810_REG_PLADCV_H, LTC6810_REG_PLADCV_L, pollADC, 1);
+//		if (status != HAL_OK){return status;}
+//	}
+
+
+	uint8_t pCell1to3VoltageBytes[numDevices*6];
+	uint8_t pCell4to6VoltageBytes[numDevices*6];
+	status = ReadRegister(LTC6810_REG_RDCVA_H, LTC6810_REG_RDCVA_L, pCell1to3VoltageBytes, numDevices*6);
+	if (status != HAL_OK){return status;}
+	status = ReadRegister(LTC6810_REG_RDCVB_H, LTC6810_REG_RDCVB_L, pCell4to6VoltageBytes, numDevices*6);
+	if (status != HAL_OK){return status;}
+
+	for (int i = 0; i < numDevices; i++){
+		for (int j = 0; j < 3; j++){
+			pCellVoltages[6*i + j] 		= (pCell1to3VoltageBytes[6*i + 2*j] << 8) | (pCell1to3VoltageBytes[6*i + 2*j + 1]);
+			pCellVoltages[6*i + j + 3] 	= (pCell4to6VoltageBytes[6*i + 2*j] << 8) | (pCell4to6VoltageBytes[6*i + 2*j + 1]);
+			// i don't know what order bytes are shifted out, the indices could be backwards (so +1 could be MSB 8 bits instead)
+			// i think first device (one directly connected to mcu) is the one whose data is received first by mcu
+		}
+	}
+
+	return status;
+}
+
+
 
 HAL_StatusTypeDef LTC6810::WriteRegister(uint8_t cmdByteHigh, uint8_t cmdByteLow, uint8_t *dataBytes, uint8_t dataLen) //length in bytes
 {	// writes the same bytes (dataBytes) to all devices at the register defined by the cmd bytes
@@ -73,7 +118,23 @@ HAL_StatusTypeDef LTC6810::ReadRegister(uint8_t cmdByteHigh, uint8_t cmdByteLow,
 	status = HAL_SPI_TransmitReceive(phspi, transmitBytes, receiveBytes, 36, HAL_MAX_DELAY);
 	if (status != HAL_OK) {return status;}
 
-//	HAL_SPI_TransmitReceive(hspi, pTxData, pRxData, Size, Timeout)
+	return status;
+}
+
+HAL_StatusTypeDef LTC6810::SendCommand(uint8_t cmdByteHigh, uint8_t cmdByteLow){ // only for initiating ADC conversions and polling if ADC is done
+	uint8_t transmitBytes[4];
+	HAL_StatusTypeDef status;
+
+	// command bytes and their PEC:
+	transmitBytes[0] = cmdByteHigh;
+	transmitBytes[1] = cmdByteLow;
+
+	uint16_t cmdPEC = calculatePEC(transmitBytes, 2);
+	transmitBytes[2] = (uint8_t) (cmdPEC >> 8);
+	transmitBytes[3] = (uint8_t) (cmdPEC & 0x00FF);
+
+	status = HAL_SPI_Transmit(phspi, transmitBytes, 6*numDevices, HAL_MAX_DELAY);
+	if (status != HAL_OK) {return status;}
 
 	return status;
 }
@@ -81,7 +142,7 @@ HAL_StatusTypeDef LTC6810::ReadRegister(uint8_t cmdByteHigh, uint8_t cmdByteLow,
 HAL_StatusTypeDef LTC6810::WakeDevices()
 {
 	uint8_t dummyData[36];
-	return ReadRegister(0x00, 0x20, dummyData, 36);
+	return ReadRegister(LTC6810_REG_RDCFG_H, LTC6810_REG_RDCFG_L, dummyData, 36);
 }
 
 void LTC6810::InitPEC15Table()
