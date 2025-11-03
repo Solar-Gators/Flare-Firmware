22a848c81ad33fc6bc1d8073dce19011ede4e08c
Justin Lopez <justincolelopez1@gmail.com>
2025-11-03 18:50:13 -0500

    adding telem project

---
+++ firmware/Telemetry/user/inc/user_threads.hpp
@@ -0,0 +1,15 @@
+#ifndef USER_THREADS_HPP
+#define USER_THREADS_HPP
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    void StartDefaultTask_user(void *argument);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
