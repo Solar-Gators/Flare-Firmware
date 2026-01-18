c121589338cacb785f780cbfae8353f37a371b7b
Justin Lopez <justincolelopez1@gmail.com>
2026-01-18 16:06:28 -0500

    blink led

--- firmware/Rear-VCU/user/src/user_threads.cpp
+++ firmware/Rear-VCU/user/src/user_threads.cpp
@@ -3,10 +3,13 @@
 #include <cmsis_os2.h>
 #include <stm32u5xx_hal.h>
 
+#include "main.h"
+
 void StartDefaultTask_user(void* argument)
 {
     for (;;)
     {
+        HAL_GPIO_TogglePin(OK_LED_GPIO_Port, OK_LED_Pin);
         osDelay(500);
     }
 }
