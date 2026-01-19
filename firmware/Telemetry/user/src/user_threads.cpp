e742c12fac92c6f2fe8bd642104848a8e29f7dc2
U-NATEACHINGER\natha <nathan.achinger@ufl.edu>
2026-01-19 11:35:55 -0500

    blinks 12V outputs for testing

--- firmware/Telemetry/user/src/user_threads.cpp
+++ firmware/Telemetry/user/src/user_threads.cpp
@@ -3,11 +3,17 @@
 #include <cmsis_os2.h>
 #include <stm32u5xx_hal.h>
 
+#include "main.h"
+
 void StartDefaultTask_user(void* argument)
 {
     while (1)
     {
-        osDelay(500);
+        HAL_GPIO_TogglePin(OK_LED_GPIO_Port, OK_LED_Pin);
+        HAL_GPIO_TogglePin(RL_CTRL_GPIO_Port, RL_CTRL_Pin);
+        HAL_GPIO_TogglePin(RR_CTRL_GPIO_Port, RR_CTRL_Pin);
+        HAL_GPIO_TogglePin(STROBE_CTRL_GPIO_Port, STROBE_CTRL_Pin);
+        osDelay(1000);
     }
 }
 
