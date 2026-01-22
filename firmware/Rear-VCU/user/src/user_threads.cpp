7eb40b96a1c911ca81ee1b5ba556aff9c716ebea
Justin Lopez <116849803+justinlopez1@users.noreply.github.com>
2026-01-21 20:10:10 -0500

    Rearvcu dev (#17)
    * Added start of CAN driver and API
    
    * modified CAN startup
    
    * modifed filter addition -> add filters then start CAN device
    
    * adding empty type when CAN not enabled
    
    * Changed from singleton impl
    
    * modified callback addition and header formatting
    
    * changing Canmsg to Canframe
    
    * added message sending
    
    * adding more sample code
    
    * some quick fixes
    
    * tested and working on bxCAN networks
    
    * fixed mistake where queues held pointers not data
    
    * fix compile error
    
    * namespace, made some functions private according to those comments
    
    * change so that send copies the canframe object for simplicity, no mtx needed. Need to look at SG_CAN macros cause i think they are getting passed straight to stm functions and they may not match correctly, maybe use enum or something which would also assert correct types being passed in?
    
    * added stuff for data length code, user now passes in an enum class to assert one of the right options and it should get mapped correctly. looking at rtr and id next
    
    * made updates to other parameters to use class enums now
    
    * clang tidy small stuff
    
    * moved some minor stuff around, added a comment on send, removed static members to prevent but with multiple can devices, now each will just make a new thread. I think there were some bugs with multiple can devices so i did this simple solution but long term it would be best to have one static thread
    
    * added todo
    
    * small fixes warned by chat
    
    * clang tidy small stuff again
    
    * blink led
    
    * started CAN stuff
    
    * fix compile error
    
    * mitsuba request
    
    * add interpreting message from steering and watchdog kicking and made thread for outputs
    
    * added noreturn to threads
    
    * main functionality should be done except for throttle
    
    * add outputs thread
    
    * small fixes like recieving power eco over can
    
    ---------
    
    Co-authored-by: Jackb18b <jackb18b@gmail.com>

--- firmware/Rear-VCU/user/src/user_threads.cpp
+++ firmware/Rear-VCU/user/src/user_threads.cpp
@@ -3,10 +3,161 @@
 #include <cmsis_os2.h>
 #include <stm32u5xx_hal.h>
 
-void StartDefaultTask_user(void* argument)
+#include "can.hpp"
+#include "main.h"
+#include "rearvcu_state.h"
+
+#define ARRAY_PRECHARGE_HOLD_TIME_MS 500
+
+// initialize devices here and such
+void init_user()
+{
+    // assert atomics work
+    static_assert(std::atomic<uint32_t>::is_always_lock_free);
+    static_assert(std::atomic<uint16_t>::is_always_lock_free);
+    static_assert(std::atomic<uint8_t>::is_always_lock_free);
+
+    // can
+    can_init();
+
+    // watchdog init
+    HAL_GPIO_WritePin(WATCHDOG_SET1_GPIO_Port,
+                      WATCHDOG_SET1_Pin,
+                      GPIO_PIN_SET);  // set to 0b10 mode to use ratio of 8
+    HAL_GPIO_WritePin(WATCHDOG_SET0_GPIO_Port,
+                      WATCHDOG_SET0_Pin,
+                      GPIO_PIN_RESET);  // 8 * 5 = 40ms timeout and needs to be kicked before
+    HAL_GPIO_WritePin(WATCHDOG_INPUT_GPIO_Port, WATCHDOG_INPUT_Pin, GPIO_PIN_SET);
+    HAL_Delay(1);
+    HAL_GPIO_WritePin(
+        WATCHDOG_ENABLE_GPIO_Port, WATCHDOG_ENABLE_Pin, GPIO_PIN_SET);  // enable watchdog
+
+    // array contactors should start open
+    HAL_GPIO_WritePin(PRE_ARRAY_CTRL_GPIO_Port, PRE_ARRAY_CTRL_Pin, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(MAIN_ARRAY_CTRL_GPIO_Port, MAIN_ARRAY_CTRL_Pin, GPIO_PIN_RESET);
+
+    // turn on mc
+    HAL_GPIO_WritePin(MC_MAIN_CTRL_GPIO_Port, MC_MAIN_CTRL_Pin, GPIO_PIN_SET);
+
+    // TODO: initialize ina chip here
+}
+
+[[noreturn]] void startHeartbeatTask_user(void* argument)
 {
     for (;;)
     {
-        osDelay(500);
+        // blink ok led
+        HAL_GPIO_TogglePin(OK_LED_GPIO_Port, OK_LED_Pin);
+
+        // send mitsuba request for frame0 to get wheel rpm, shouldnt be sent faster than every 500ms
+        can_device.Send(mitsuba_frame0_request);
+        ++vcu_state.can_messages_sent;
+
+        osDelay(550);
+    }
+}
+
+[[noreturn]] void startRegenThrottleTask_user(void* argument)
+{
+    for (;;)
+    {
+        // kick watchdog by sending a falling edge
+        HAL_GPIO_WritePin(WATCHDOG_INPUT_GPIO_Port, WATCHDOG_INPUT_Pin, GPIO_PIN_RESET);
+        HAL_Delay(1);
+        HAL_GPIO_WritePin(WATCHDOG_INPUT_GPIO_Port, WATCHDOG_INPUT_Pin, GPIO_PIN_SET);
+
+        // consistently update throttle and regen here based on..
+        // throttle value over can
+        // regen value over can
+        // cruise control stuff
+        osDelay(25);
+    }
+}
+
+// should continuously write to pins like power/eco, direction, contactors, and send CAN message to tell car that mc is enabled and such
+[[noreturn]] void startOutputsTask_user(void* argument)
+{
+    sg::CANFrame rearvcu_statuses_frame{0x020,
+                                        sg::CANFrameIDType::STANDARD,
+                                        sg::CANFrameRTRMode::DATA,
+                                        sg::CANFrameLen::BYTES_16,
+                                        0,
+                                        {}};
+
+    ArrayContactors array_contactors = ArrayContactors::BOTH_OPEN;
+    uint32_t precharge_closed_timestamp = 0;
+
+    for (;;)
+    {
+        /*
+        // test block for outputs
+        HAL_GPIO_TogglePin(MC_MAIN_CTRL_GPIO_Port, MC_MAIN_CTRL_Pin);
+        HAL_GPIO_TogglePin(MC_PWR_ECO_CTRL_GPIO_Port, MC_PWR_ECO_CTRL_Pin);
+        HAL_GPIO_TogglePin(MC_FWD_REV_CTRL_GPIO_Port, MC_FWD_REV_CTRL_Pin);
+        HAL_GPIO_TogglePin(MAIN_ARRAY_CTRL_GPIO_Port, MAIN_ARRAY_CTRL_Pin);
+        HAL_GPIO_TogglePin(PRE_ARRAY_CTRL_GPIO_Port, PRE_ARRAY_CTRL_Pin);
+        */
+
+        // TODO: can make these writes less frequent using flag, only call writepin on change yk
+        // power eco pin
+        MCPowerMode mc_power_mode_requested = vcu_state.mc_power_mode_requested.load();
+        HAL_GPIO_WritePin(MC_PWR_ECO_CTRL_GPIO_Port,
+                          MC_PWR_ECO_CTRL_Pin,
+                          static_cast<GPIO_PinState>(mc_power_mode_requested));
+
+        // direction pin
+        Direction direction_requested = vcu_state.direction_requested.load();
+        HAL_GPIO_WritePin(MC_FWD_REV_CTRL_GPIO_Port,
+                          MC_FWD_REV_CTRL_Pin,
+                          static_cast<GPIO_PinState>(direction_requested));
+
+        // array contactors logic
+        // TODO: user timer peripheral for consistent timer logic
+        if (vcu_state.array_contactors_requested_closed.load())
+        {
+            if (array_contactors == ArrayContactors::BOTH_OPEN)
+            {
+                // close pre
+                HAL_GPIO_WritePin(PRE_ARRAY_CTRL_GPIO_Port, PRE_ARRAY_CTRL_Pin, GPIO_PIN_SET);
+                precharge_closed_timestamp = HAL_GetTick();
+                array_contactors = ArrayContactors::PRECHARGE_CLOSED;
+            }
+            else if (array_contactors == ArrayContactors::PRECHARGE_CLOSED &&
+                     HAL_GetTick() - precharge_closed_timestamp > ARRAY_PRECHARGE_HOLD_TIME_MS)
+            {
+                // close main
+                HAL_GPIO_WritePin(MAIN_ARRAY_CTRL_GPIO_Port, MAIN_ARRAY_CTRL_Pin, GPIO_PIN_SET);
+                array_contactors = ArrayContactors::MAIN_CLOSED;
+            }
+        }
+        else
+        {
+            // open both contactors
+            HAL_GPIO_WritePin(PRE_ARRAY_CTRL_GPIO_Port, PRE_ARRAY_CTRL_Pin, GPIO_PIN_SET);
+            HAL_GPIO_WritePin(MAIN_ARRAY_CTRL_GPIO_Port, MAIN_ARRAY_CTRL_Pin, GPIO_PIN_SET);
+            array_contactors = ArrayContactors::BOTH_OPEN;
+        }
+
+        // TODO: could get voltage of supp batt here
+        uint16_t supp_batt_voltage_mv = 0x0F0F;  // dummy
+
+        // TODO: could get current draw of supp batt here
+        uint16_t supp_batt_current = 0x0F0F;  // dummy
+
+        // setup and send diagnostic can message
+        rearvcu_statuses_frame.data[0] =
+            1;  // should always be enabled if this board is alive, written to at startup
+        rearvcu_statuses_frame.data[1] = static_cast<uint8_t>(direction_requested);
+        rearvcu_statuses_frame.data[2] = static_cast<uint8_t>(mc_power_mode_requested);
+        rearvcu_statuses_frame.data[3] = static_cast<uint8_t>(array_contactors);
+        rearvcu_statuses_frame.data[4] = static_cast<uint8_t>(supp_batt_voltage_mv);       // lsb
+        rearvcu_statuses_frame.data[5] = static_cast<uint8_t>(supp_batt_voltage_mv >> 8);  // msb
+        rearvcu_statuses_frame.data[6] = static_cast<uint8_t>(supp_batt_current);          // lsb
+        rearvcu_statuses_frame.data[7] = static_cast<uint8_t>(supp_batt_current >> 8);     // msb
+        rearvcu_statuses_frame.data[8] = vcu_state.car_speed.load();
+        can_device.Send(rearvcu_statuses_frame);
+
+        osDelay(1000);
+        //osDelay(60);
     }
 }
