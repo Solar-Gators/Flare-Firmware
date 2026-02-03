b456051be6ecefd31ece68fa04005e6aa1c5faae
Justin Lopez <justincolelopez1@gmail.com>
2026-02-02 19:13:27 -0500

    temp

--- firmware/Telemetry/user/inc/can.h
+++ firmware/Telemetry/user/inc/can.h
@@ -6,3 +6,7 @@
 extern FDCAN_HandleTypeDef hfdcan1;
 
 inline sg::CANDevice can_device(&hfdcan1);
+
+void can_init();
+
+void steeringRequestsCallback(sg::CANFrame frame);
--- firmware/Telemetry/user/src/can.cpp
+++ firmware/Telemetry/user/src/can.cpp
@@ -1,3 +1,10 @@
 #include "can.h"
 
 #include "CanDriver.hpp"
+
+void can_init()
+{
+    can_device.addCallbackId() can_device.StartCANDevice();
+}
+
+void steeringRequestsCallback() {}
