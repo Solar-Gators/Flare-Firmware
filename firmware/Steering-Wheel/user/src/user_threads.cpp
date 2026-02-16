44cf4c602d5527f1e3e16c6bef4da978bfd7943a
Richard-Carnivale <145724604+Richard-Carnivale@users.noreply.github.com>
2026-02-16 17:26:10 -0500

    fixed rotation/text size and added info to the screen

--- firmware/Steering-Wheel/user/src/user_threads.cpp
+++ firmware/Steering-Wheel/user/src/user_threads.cpp
@@ -29,26 +29,42 @@ void startScreenTask_user(void* argument)
 
     ILI9341 display(240, 320);
     display.Init();
-    display.ClearScreen(RGB565_RED);
+    display.SetRotation(3);
+    display.ClearScreen(RGB565_ORANGE);
+    display.SetTextSize(2);
     //display.FillRect(220, 0, 20, 10, RGB565_GREEN);
-    uint8_t speed = steering_state.car_speed.load(); // delete after testing
+    // bool screen_is_switched = false; // starting screen (just an idea for different screens with different info)
+    char buf[6];
 
     for (;;)
     {
-        //display.FillRect(0, 0, 20, 10, RGB565_RED);
-
-        // uint8_t speed = steering_state.car_speed.load();
-        speed++;
 
         // TODO: create indicators for the right buttons to turn on as the lights on the actual buttons arent working
-        // TODO: important info, such as cruise control and regenerative breaking
+        // TODO: important info: cruise control, regenerative breaking, car speed, array connectors, sup batt voltage
 
-        // display.SetTextSize(uint8_t size);
-        char buf[4];
+        uint8_t speed = steering_state.car_speed.load();
+        uint16_t sup_batt_volt = steering_state.supp_batt_voltage.load();
+        Direction direction = steering_state.direction_requested.load();
+
+        speed++; //remove after testing
+
         itoa(speed, buf, 10);
-        display.DrawText(150, 150, buf, RGB565_GREEN);
-        // use fill rect to clear old values
+        display.DrawText(5, 20, "Speed: ", RGB565_BLUE);
+        display.DrawText(75, 20, buf, RGB565_BLUE);
 
+        itoa(sup_batt_volt, buf, 10);
+        display.DrawText(5, 40, "Sup volt: ", RGB565_BLUE);
+        display.DrawText(115, 40, buf, RGB565_BLUE);
+
+        display.DrawText(5, 60, "Direction: ", RGB565_BLUE);
+        if (static_cast<int>(direction) == 1)
+            display.DrawText(130, 60, "Forward", RGB565_BLUE);
+        else if (static_cast<int>(direction) == 0)
+            display.DrawText(130, 60, "Backward", RGB565_BLUE);
+        else
+            display.DrawText(130, 60, "ERROR", RGB565_BLUE);
+
+        // snprintf causes hard fault
         // maybe different screens
 
         osDelay(500);
