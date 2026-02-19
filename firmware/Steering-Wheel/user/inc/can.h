23b72f32a91c40e15605376c506e496a79fbc543
Justin Lopez <justincolelopez1@gmail.com>
2026-02-18 21:59:01 -0500

    make speed and sup volt show up on screen even though they aren't done yet yk

--- firmware/Steering-Wheel/user/inc/can.h
+++ firmware/Steering-Wheel/user/inc/can.h
@@ -11,6 +11,7 @@ inline sg::CANDevice can_device(&hfdcan1);
 void can_init();
 
 HAL_StatusTypeDef rearVCUInfoMessageCallback(const sg::CANFrame& msg, void* ctx);
+HAL_StatusTypeDef rearVCUSuppBattMessageCallback(const sg::CANFrame& msg, void* ctx);
 HAL_StatusTypeDef bmsBatteryVoltageMessageCallback(const sg::CANFrame& msg, void* ctx);
 HAL_StatusTypeDef bmsBatteryTempMessageCallback(const sg::CANFrame& msg, void* ctx);
 
--- firmware/Steering-Wheel/user/src/can.cpp
+++ firmware/Steering-Wheel/user/src/can.cpp
@@ -21,6 +21,8 @@ void can_init()
     ASSERT_TRUE(can_device.addCallbackId(
         0x020, sg::CANFrameIDType::STANDARD, &rearVCUInfoMessageCallback, nullptr));
     ASSERT_TRUE(can_device.addCallbackId(
+        0x021, sg::CANFrameIDType::STANDARD, &rearVCUSuppBattMessageCallback, nullptr));
+    ASSERT_TRUE(can_device.addCallbackId(
         0x041, sg::CANFrameIDType::STANDARD, &bmsBatteryVoltageMessageCallback, nullptr));
     ASSERT_TRUE(can_device.addCallbackId(
         0x042, sg::CANFrameIDType::STANDARD, &bmsBatteryTempMessageCallback, nullptr));
@@ -35,10 +37,14 @@ HAL_StatusTypeDef rearVCUInfoMessageCallback(const sg::CANFrame& msg, void* ctx)
     steering::state.actual_array_contactors_status.store(
         static_cast<flare_can::ArrayContactors>(msg.data[3]), std::memory_order_relaxed);
 
-    uint16_t supp_batt_voltage = (msg.data[4]) | (static_cast<uint16_t>(msg.data[5]) << 8);
-    steering::state.supp_batt_voltage_mv.store(supp_batt_voltage, std::memory_order_relaxed);
+    steering::state.car_speed.store(msg.data[4], std::memory_order_relaxed);
 
-    steering::state.car_speed.store(msg.data[8], std::memory_order_relaxed);
+    return HAL_OK;
+}
+HAL_StatusTypeDef rearVCUSuppBattMessageCallback(const sg::CANFrame& msg, void* ctx)
+{
+    uint16_t supp_batt_voltage = (msg.data[0]) | (static_cast<uint16_t>(msg.data[1]) << 8);
+    steering::state.supp_batt_voltage_mv.store(supp_batt_voltage, std::memory_order_relaxed);
 
     return HAL_OK;
 }
