7cf3a1105a0b607443de51be74c7e107964edca3
Justin Lopez <justincolelopez1@gmail.com>
2026-04-07 15:36:02 -0400

    throttle working! needed to resolder can chip on rear vcu. sending from front to rear and being outputted on dac. op amp still not working right though

--- firmware/Front-VCU/Core/Inc/stm32u5xx_it.h
+++ firmware/Front-VCU/Core/Inc/stm32u5xx_it.h
@@ -54,6 +54,7 @@ void UsageFault_Handler(void);
 void DebugMon_Handler(void);
 void GPDMA1_Channel0_IRQHandler(void);
 void ADC1_IRQHandler(void);
+void FDCAN1_IT0_IRQHandler(void);
 void TIM1_UP_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
--- firmware/Front-VCU/Core/Src/stm32u5xx_it.c
+++ firmware/Front-VCU/Core/Src/stm32u5xx_it.c
@@ -59,6 +59,7 @@ extern DMA_NodeTypeDef Node_GPDMA1_Channel0;
 extern DMA_QListTypeDef List_GPDMA1_Channel0;
 extern DMA_HandleTypeDef handle_GPDMA1_Channel0;
 extern ADC_HandleTypeDef hadc1;
+extern FDCAN_HandleTypeDef hfdcan1;
 extern TIM_HandleTypeDef htim1;
 
 /* USER CODE BEGIN EV */
@@ -189,6 +190,20 @@ void ADC1_IRQHandler(void)
   /* USER CODE BEGIN ADC1_IRQn 1 */
 
   /* USER CODE END ADC1_IRQn 1 */
+}
+
+/**
+  * @brief This function handles FDCAN1 interrupt 0.
+  */
+void FDCAN1_IT0_IRQHandler(void)
+{
+  /* USER CODE BEGIN FDCAN1_IT0_IRQn 0 */
+
+  /* USER CODE END FDCAN1_IT0_IRQn 0 */
+  HAL_FDCAN_IRQHandler(&hfdcan1);
+  /* USER CODE BEGIN FDCAN1_IT0_IRQn 1 */
+
+  /* USER CODE END FDCAN1_IT0_IRQn 1 */
 }
 
 /**
