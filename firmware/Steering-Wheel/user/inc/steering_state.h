59e32b3d857456830f2c515d0f8604b9843b742b
Richard-Carnivale <145724604+Richard-Carnivale@users.noreply.github.com>
2026-05-08 16:16:46 -0400

    clang

--- firmware/Steering-Wheel/user/inc/steering_state.h
+++ firmware/Steering-Wheel/user/inc/steering_state.h
@@ -13,7 +13,8 @@
 struct SteeringState
 {
     // sending
-    std::atomic<uint8_t> regen_percent_requested{};   // 0 - 255 as percent so 255 = 100% regen strength
+    std::atomic<uint8_t>
+        regen_percent_requested{};            // 0 - 255 as percent so 255 = 100% regen strength
     std::atomic<bool> is_cc_on{};             // determines if cc is active
     std::atomic<uint8_t> cc_mph_requested{};  // in mph
     std::atomic<bool> array_contactors_requested_closed{};
