59e72113e348095737e7c384a589fd4b0019d79d
Nate <nathan.achinger@ufl.edu>
2026-05-18 16:27:54 -0400

    mppt callbacks, cmake config, fixed some CAN driver issues with standard mode

--- firmware/Telemetry-Sensors/user/inc/can.h
+++ firmware/Telemetry-Sensors/user/inc/can.h
@@ -14,16 +14,22 @@ extern CAN_HandleTypeDef hcan1;
 
 void canInit();
 
-HAL_StatusTypeDef GenericFrameForwardingCallback(const sg::CANFrame& frame, void* ctx);
+HAL_StatusTypeDef MPPT1InputMeasurementsCallback(const sg::CANFrame& frame, void* ctx);
+HAL_StatusTypeDef MPPT2InputMeasurementsCallback(const sg::CANFrame& frame, void* ctx);
+HAL_StatusTypeDef MPPT3InputMeasurementsCallback(const sg::CANFrame& frame, void* ctx);
+
+HAL_StatusTypeDef MPPT1OutputMeasurementsCallback(const sg::CANFrame& frame, void* ctx);
+HAL_StatusTypeDef MPPT2OutputMeasurementsCallback(const sg::CANFrame& frame, void* ctx);
+HAL_StatusTypeDef MPPT3OutputMeasurementsCallback(const sg::CANFrame& frame, void* ctx);
 
 inline sg::CANDevice can_device(&hcan1,
                                 {
-                                    {MPPT1_BASE_ADDR, {&GenericFrameForwardingCallback}},
-                                    {MPPT2_BASE_ADDR, {&GenericFrameForwardingCallback}},
-                                    {MPPT3_BASE_ADDR, {&GenericFrameForwardingCallback}},
-                                    {MPPT1_BASE_ADDR + 1, {&GenericFrameForwardingCallback}},
-                                    {MPPT2_BASE_ADDR + 1, {&GenericFrameForwardingCallback}},
-                                    {MPPT3_BASE_ADDR + 1, {&GenericFrameForwardingCallback}},
+                                    {MPPT1_BASE_ADDR, {&MPPT1InputMeasurementsCallback}},
+                                    {MPPT2_BASE_ADDR, {&MPPT2InputMeasurementsCallback}},
+                                    {MPPT3_BASE_ADDR, {&MPPT3InputMeasurementsCallback}},
+                                    {MPPT1_BASE_ADDR + 1, {&MPPT1OutputMeasurementsCallback}},
+                                    {MPPT2_BASE_ADDR + 1, {&MPPT2OutputMeasurementsCallback}},
+                                    {MPPT3_BASE_ADDR + 1, {&MPPT3OutputMeasurementsCallback}},
                                 },
                                 {});
 
--- firmware/Telemetry-Sensors/user/src/can.cpp
+++ firmware/Telemetry-Sensors/user/src/can.cpp
@@ -1,1 +1,87 @@
 #include "can.h"
+
+#include "sensor_data.h"
+
+HAL_StatusTypeDef MPPT1InputMeasurementsCallback(const sg::CANFrame& frame, void* ctx)
+{
+    float voltage = 0.0f;
+    float current = 0.0f;
+
+    std::memcpy(&voltage, &frame.data[0], sizeof(float));
+    std::memcpy(&current, &frame.data[4], sizeof(float));
+
+    mppt1.input.voltage.store(voltage, std::memory_order_relaxed);
+    mppt1.input.current.store(current, std::memory_order_relaxed);
+
+    return HAL_OK;
+}
+
+HAL_StatusTypeDef MPPT2InputMeasurementsCallback(const sg::CANFrame& frame, void* ctx)
+{
+    float voltage = 0.0f;
+    float current = 0.0f;
+
+    std::memcpy(&voltage, &frame.data[0], sizeof(float));
+    std::memcpy(&current, &frame.data[4], sizeof(float));
+
+    mppt2.input.voltage.store(voltage, std::memory_order_relaxed);
+    mppt2.input.current.store(current, std::memory_order_relaxed);
+
+    return HAL_OK;
+}
+
+HAL_StatusTypeDef MPPT3InputMeasurementsCallback(const sg::CANFrame& frame, void* ctx)
+{
+    float voltage = 0.0f;
+    float current = 0.0f;
+
+    std::memcpy(&voltage, &frame.data[0], sizeof(float));
+    std::memcpy(&current, &frame.data[4], sizeof(float));
+
+    mppt3.input.voltage.store(voltage, std::memory_order_relaxed);
+    mppt3.input.current.store(current, std::memory_order_relaxed);
+
+    return HAL_OK;
+}
+
+HAL_StatusTypeDef MPPT1OutputMeasurementsCallback(const sg::CANFrame& frame, void* ctx)
+{
+    float voltage = 0.0f;
+    float current = 0.0f;
+
+    std::memcpy(&voltage, &frame.data[0], sizeof(float));
+    std::memcpy(&current, &frame.data[4], sizeof(float));
+
+    mppt1.output.voltage.store(voltage, std::memory_order_relaxed);
+    mppt1.output.current.store(current, std::memory_order_relaxed);
+
+    return HAL_OK;
+}
+
+HAL_StatusTypeDef MPPT2OutputMeasurementsCallback(const sg::CANFrame& frame, void* ctx)
+{
+    float voltage = 0.0f;
+    float current = 0.0f;
+
+    std::memcpy(&voltage, &frame.data[0], sizeof(float));
+    std::memcpy(&current, &frame.data[4], sizeof(float));
+
+    mppt2.output.voltage.store(voltage, std::memory_order_relaxed);
+    mppt2.output.current.store(current, std::memory_order_relaxed);
+
+    return HAL_OK;
+}
+
+HAL_StatusTypeDef MPPT3OutputMeasurementsCallback(const sg::CANFrame& frame, void* ctx)
+{
+    float voltage = 0.0f;
+    float current = 0.0f;
+
+    std::memcpy(&voltage, &frame.data[0], sizeof(float));
+    std::memcpy(&current, &frame.data[4], sizeof(float));
+
+    mppt3.output.voltage.store(voltage, std::memory_order_relaxed);
+    mppt3.output.current.store(current, std::memory_order_relaxed);
+
+    return HAL_OK;
+}
