c4a7ea9215dbfdf36c3e8ea7549245070cdf7562
Richard-Carnivale <145724604+Richard-Carnivale@users.noreply.github.com>
2026-06-10 17:18:23 -0400

    changed all cmake lists and added front vcu watchdog
    needs to be tested on board

--- firmware/Front-VCU/user/src/user_threads.cpp
+++ firmware/Front-VCU/user/src/user_threads.cpp
@@ -5,6 +5,7 @@
 
 #include "frontvcu.h"
 #include "main.h"
+#include "watchdog.hpp"
 
 #include <atomic>
 
@@ -15,55 +16,79 @@ void init_user()
 
 void StartHeartbeat_user(void* argument)
 {
+    sg::Watchdog wdog1;
+    wdog1.IWDG_Init();
+
     for (;;)
     {
         HAL_GPIO_TogglePin(OK_LED_GPIO_Port, OK_LED_Pin);
+        wdog1.Kick();
         osDelay(500);
     }
 }
 
 void StartCANMessagesTX_user(void* argument)
 {
+    sg::Watchdog wdog2;
+    wdog2.IWDG_Init();
+
     for (;;)
     {
         frontvcu::sendCANMessagesTX();
+        wdog2.Kick();
         osDelay(20);
     }
 }
 
 void StartLoadsControl_user(void* argument)
 {
+    sg::Watchdog wdog3;
+    wdog3.IWDG_Init();
+
     for (;;)
     {
         frontvcu::writeLoadsControl();
+        wdog3.Kick();
         osDelay(30);
     }
 }
 
 void StartCurrentSense_user(void* argument)
 {
+    sg::Watchdog wdog4;
+    wdog4.IWDG_Init();
+
     for (;;)
     {
         frontvcu::readCurrentSense();
         frontvcu::processHornAndFan();
+        wdog4.Kick();
         osDelay(40);
     }
 }
 
 void StartBreakSense_user(void* argument)
 {
+    sg::Watchdog wdog5;
+    wdog5.IWDG_Init();
+
     for (;;)
     {
         frontvcu::readBrakeSense();
+        wdog5.Kick();
         osDelay(15);
     }
 }
 
 void StartCalculateCC_user(void* argument)
 {
+    sg::Watchdog wdog6;
+    wdog6.IWDG_Init();
+
     for (;;)
     {
         frontvcu::updateThrottleCommand();
+        wdog6.Kick();
         osDelay(19);
     }
 }
